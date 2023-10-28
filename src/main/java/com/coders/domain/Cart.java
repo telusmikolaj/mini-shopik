9109821a777e2361477ab94bf7ded7d91efad612
joannawalach1 <joanna.walach@op.pl>
2023-10-28 14:38:08 +0200

    created entites cart product and user java

--- src/main/java/com/coders/domain/Cart.java
+++ src/main/java/com/coders/domain/Cart.java
@@ -1,24 +1,70 @@
 package com.coders.domain;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.util.List;
+import java.util.Objects;
 
-@Data
-@Entity(name = "CART")
 public class Cart {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "user_id")
     private User user;
-    @OneToMany(mappedBy = "cart")
     private List<Product> productList;
 
-    public Cart(User user, List<Product> productList) {
+    public Cart(Long id, User user, List<Product> productList) {
+        this.id = id;
         this.user = user;
         this.productList = productList;
+    }
+
+    public Cart() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public void addProduct(Product product) {
+        product.setCart(this);
+        productList.add(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart cart = (Cart) o;
+        return Objects.equals(id, cart.id) && Objects.equals(user, cart.user) && Objects.equals(productList, cart.productList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, productList);
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id=" + id +
+                ", user=" + user +
+                ", productList=" + productList +
+                '}';
     }
 }
--- src/main/java/com/coders/domain/User.java
+++ src/main/java/com/coders/domain/User.java
@@ -1,23 +1,11 @@
 package com.coders.domain;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
-@Data
-@Entity(name = "USER")
-@Getter
-@Setter
-@NoArgsConstructor
-public class User implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class User {
+
     private Long id;
     private String firstName;
     private String lastName;
@@ -26,7 +14,6 @@ public class User implements Serializable {
     private String role;
     private BigDecimal accountBalance;
 
-    @OneToMany(mappedBy = "user")
     private List<Cart> carts;
 
     public User(String firstName, String lastName, String login, String password, String role, BigDecimal accountBalance) {
@@ -36,5 +23,103 @@ public class User implements Serializable {
         this.password = password;
         this.role = role;
         this.accountBalance = accountBalance;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public List<Cart> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(List<Cart> carts) {
+        this.carts = carts;
+    }
+
+    public void addCart(Cart cart) {
+        carts.add(cart);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(login, user.login) && Objects.equals(password, user.password) && Objects.equals(role, user.role) && Objects.equals(accountBalance, user.accountBalance) && Objects.equals(carts, user.carts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, login, password, role, accountBalance, carts);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                ", accountBalance=" + accountBalance +
+                ", carts=" + carts +
+                '}';
     }
 }
